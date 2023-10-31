d741faf20397bf9198a2fffb7c22172cfb0d6a77
bluthej <joffrey.bluthe@e.email>
2023-10-31 10:02:03 +0100

    Fix RGB underlight

--- rules.mk
+++ rules.mk
@@ -1,5 +1,7 @@
 BOOTMAGIC_ENABLE = yes
 MOUSEKEY_ENABLE = yes    # Mouse keys
+RGB_MATRIX_ENABLE = no
+RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
 OLED_ENABLE     = yes
 LTO_ENABLE      = yes
 # Bootloader selection
